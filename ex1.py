10240849c79369868bb5fcd9f11ab3b240b6f3bd
aiden-chanyoung <chanyoung_park@naver.com>
2020-09-29 22:39:18 +0900

    update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,5 +1,12 @@
 
 import tensorflow as tf
-hello = tf.constant('Hello, YEHS :)')
 sess = tf.Session()
+
+# Print Example
+hello = tf.constant('Hello, YEHS :)')
 print(sess.run(hello).decode())
+
+# Calculate Example
+a = tf.constant(40)
+b = tf.constant(25)
+print(sess.run(a+b))
