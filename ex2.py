41d15a1c78cf97323e74763c3527434abd061a79
aiden-chanyoung <chanyoung_park@naver.com>
2020-09-30 09:34:55 +0900

    chanyoung : original ex2.py

--- ex2.py
+++ ex2.py
@@ -6,7 +6,3 @@ YEHS = tf.constant('YEHS')
 print(sess.run(YEHS).decode())
 
 # Add your name and print it :)
-CYPARK = tf.constant('Chan Young Park')
-print(sess.run(CYPARK).decode())
-AIDEN = tf.constant('Aiden')
-print(sess.run(AIDEN).decode())
